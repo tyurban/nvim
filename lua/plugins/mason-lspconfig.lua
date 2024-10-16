3c5e0d5ffca3218a637729a8813e19fe346b528f
Tyler Urban <turban@knackworks.com>
2024-10-16 13:47:44 -0400

    setup auto completion

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -3,12 +3,15 @@ return {
   dependencies = {
     "williamboman/mason.nvim",
     "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp"
   },
   opts = {
-    ensure_installed = { "lua_ls" },
+    ensure_installed = { "lua_ls", "pyright" },
     handlers = {
       function(server_name)
-        require("lspconfig")[server_name].setup({})
+        require("lspconfig")[server_name].setup({
+	  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        })
       end,
     },
   },
---
+++ lua/plugins/nvim-snippets.lua
@@ -0,0 +1,5 @@
+return {
+  "garymjr/nvim-snippets",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  opts = { friendly_snippets = true },
+}
